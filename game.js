e14d0364472bde6c108ce90351677ec076fc622c
markopavlo69-svg <markopavlo.69@gmail.com>
2026-03-15 07:01:38 +0100

    made user input case insensitive works made if else logic but it always returns into a tie

--- game.js
+++ game.js
@@ -1,32 +1,69 @@
 // INIT
-
-
+playRound();
+console.log( "computer:",getComputerChoice());
+console.log( "human",getComputerChoice());
 //Variables
-let cScore =0;
-let hScore =0;
+let computerScore =0;
+let humanScore =0;
 
 
 
 //functions
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random()*3);
-    let choice;
+    let computerChoice;
     if(randomNumber === 0){
-        choice = "rock"
+        computerChoice = "rock"
     }
     else if (randomNumber === 1){
-        choice = "paper";
+        computerChoice = "paper";
     }
     else{
-        choice = "sciccors";
+        computerChoice = "sciccors";
     }
-    return choice
+    return computerChoice
 }
 
  function getHumanChoice(){
-    let hchoice = prompt("to play type rock, paper or sciccor");
-    console.log(hchoice);
-    return hchoice
+    let humanChoice = prompt("to play type rock, paper or sciccor");
+    humanChoice = humanChoice.toLowerCase();
+    console.log(humanChoice);
+    return humanChoice;
+
+}
+
+function playRound(humanChoice, computerChoice){
+    getHumanChoice();
+    getComputerChoice();
+    
+
+    if(humanChoice === "rock" && computerChoice === "paper" ){
+        console.log("computer wins")
+        computerScore++;
+    }
+    else if (humanChoice === "rock" && computerChoice === "sciccor"){
+        console.log("you win")
+        humanScore++
+    }
+    else if(humanChoice === "paper" && computerChoice === "rock"){
+        console.log("you win");
+        humanScore++;
+    }
+       else if(humanChoice === "paper" && computerChoice === "sciccor"){
+        console.log("computer wins");
+        computerScore++;
+    }
+       else if(humanChoice === "sciccor" && computerChoice === "rock"){
+        console.log("computer wins");
+        computerScore++;
+    }
+       else if(humanChoice === "sciccor" && computerChoice === "paper"){
+        console.log("you win");
+        humanScore++;
+    }
+    else{
+        console.log("tie")
+    }
 
 }
 
--- plan.md
+++ plan.md
@@ -53,3 +53,21 @@ test with calling the function and console log
 SEQUENCE
 create variable cScore value 0
 create variable hScore value 0
+SEQUENCE
+create function playRound
+function has two parameters humanChoice and computer Choice
+use the toLowerCase function to make human choice case insensitive in the function getHumanchoice
+IF ELSE THEN
+make every comparrision where one wins over the another and an else block for all ties
+log the winner and increment score
+test with console log
+if else logic:
+human rock computer rock tie
+human rock computer paper computer wins DONE
+human rock computer sciccor human wins DONE
+human paper computer Rock human wins DONE
+human paper computer paper TIE
+human paper computer Sciccor Computer WINS DONE
+human sciccor computer rock computer wins DONE
+human sciccor computer paper human wins DONE
+human sciccor computer sciccor TIE
